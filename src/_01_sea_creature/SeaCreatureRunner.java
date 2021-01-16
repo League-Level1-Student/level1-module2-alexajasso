733914f57a672c1c1923eb5484c99998630ee207
root <root@84fb004fbb63>
2021-01-16 19:27:35 +0000

    Working on sea creature.

---
+++ src/_01_sea_creature/SeaCreatureRunner.java
@@ -0,0 +1,14 @@
+package _01_sea_creature;
+
+public class SeaCreatureRunner {
+
+	public static void main(String[] args) {
+		
+	SeaCreature creature = new SeaCreature("Spongebob");
+		creature.eat();
+		
+		System.out.println (creature.getName());
+		
+	}
+
+}
