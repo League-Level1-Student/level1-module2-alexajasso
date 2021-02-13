89b74cf0c62538ffb3225a69ae263bff7ba8dfa9
root <root@da8fdd4a1534>
2021-02-13 19:24:21 +0000

    i finished sea creature runner and i started smurf.

--- src/_01_sea_creature/SeaCreatureRunner.java
+++ src/_01_sea_creature/SeaCreatureRunner.java
@@ -3,12 +3,21 @@ package _01_sea_creature;
 public class SeaCreatureRunner {
 
 	public static void main(String[] args) {
-		
+
 	SeaCreature creature = new SeaCreature("Spongebob");
 		creature.eat();
 		
 		System.out.println (creature.getName());
 		
+		SeaCreature creature1 = new SeaCreature("Patrick");
+		creature1.eat();
+		
+		System.out.println (creature1.getName());
+		
+		SeaCreature creature2 = new SeaCreature("Squidward");
+		creature2.eat();
+		
+		System.out.println (creature2.getName());
 	}
 
 }
