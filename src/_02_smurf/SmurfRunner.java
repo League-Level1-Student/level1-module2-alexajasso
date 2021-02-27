2e8815dcb5e30a21612228213c7d0b646b98f384
root <root@ade17d6156af>
2021-02-27 19:19:03 +0000

    I am working on Smurf.

---
+++ src/_02_smurf/SmurfRunner.java
@@ -0,0 +1,24 @@
+package _02_smurf;
+
+import _01_sea_creature.SeaCreature;
+
+public class SmurfRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+/* 
+ * 1. Watch this Smurf cartoon: https://www.youtube.com/watch?v=7-RDGT5Lsp8 
+ * 2. In a Runner class, make a Handy Smurf. Use the methods below to make him eat, and print his name.
+ * 3. & 4. see comments in code below
+ * 5. Make a Papa Smurf and print his name, hat color and girl or boy.
+ * 6. Make a Smurfette and print her name, hat color and girl or boy. 
+ */
+	Smurf blue = new Smurf ("Smurf");
+	blue.eat();
+	
+	
+	
+	}
+
+}
